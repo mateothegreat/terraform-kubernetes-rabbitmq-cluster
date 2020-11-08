008eaac01d643cb9d76ad59e821d46716746c900
Matthew Davis <matthew@matthewdavis.io>
2020-11-08 16:00:49 -0500

    bump

--- main.tf
+++ main.tf
@@ -144,7 +144,7 @@ resource "null_resource" "readywait" {
 
     provisioner "local-exec" {
 
-        command = "sleep 10 && kubectl --server=\"${ var.server }\" --token=\"${ var.token }\" rollout status statefulset/${ var.name }-rabbitmq-server"
+        command = "sleep 10 && kubectl --server=\"${ var.host }\" --token=\"${ var.token }\" rollout status statefulset/${ var.name }-rabbitmq-server"
 
     }
 
@@ -158,7 +158,7 @@ resource "null_resource" "adduser" {
 
     provisioner "local-exec" {
 
-        command = "kubectl --server=\"${ var.server }\" --token=\"${ var.token }\" exec -n ${ var.namespace } ${ var.name }-rabbitmq-server-0 -it -- rabbitmqctl add_user '${ var.users[ count.index ].username }' '${ var.users[ count.index ].password }' && kubectl exec -n ${ var.namespace } ${ var.name }-rabbitmq-server-0 -it -- rabbitmqctl set_user_tags '${ var.users[ count.index ].username }' '${ var.users[ count.index ].tags }' && kubectl exec -n ${ var.namespace } ${ var.name }-rabbitmq-server-0 -it -- rabbitmqctl set_permissions -p / '${ var.users[ count.index ].username }' ${ var.users[ count.index ].permissions }"
+        command = "kubectl --server=\"${ var.host }\" --token=\"${ var.token }\" exec -n ${ var.namespace } ${ var.name }-rabbitmq-server-0 -it -- rabbitmqctl add_user '${ var.users[ count.index ].username }' '${ var.users[ count.index ].password }' && kubectl exec -n ${ var.namespace } ${ var.name }-rabbitmq-server-0 -it -- rabbitmqctl set_user_tags '${ var.users[ count.index ].username }' '${ var.users[ count.index ].tags }' && kubectl exec -n ${ var.namespace } ${ var.name }-rabbitmq-server-0 -it -- rabbitmqctl set_permissions -p / '${ var.users[ count.index ].username }' ${ var.users[ count.index ].permissions }"
 
     }
 
