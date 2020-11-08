fc42a412d81d03b5dd7491b070f7a4505073cdfa
Matthew Davis <matthew@matthewdavis.io>
2020-11-08 15:43:16 -0500

    first commit

---
+++ main.tf
@@ -0,0 +1,165 @@
+provider "kubernetes" {
+
+    alias = "this"
+
+    host     = var.host
+    token    = var.token
+    insecure = var.insecure
+
+}
+
+provider "kubernetes-alpha" {
+
+    alias = "this"
+
+    host     = var.host
+    token    = var.token
+    insecure = var.insecure
+
+}
+
+resource "kubernetes_manifest" "cluster" {
+
+    provider = kubernetes-alpha.this
+
+    manifest = {
+
+        "apiVersion" = "rabbitmq.com/v1beta1"
+        "kind"       = "RabbitmqCluster"
+
+        "metadata" = {
+
+            "namespace" = var.namespace
+            "name"      = var.name
+
+        }
+
+        "spec" = {
+
+            replicas = var.replicas
+            image    = "rabbitmq:3-management"
+
+            override = {
+
+                clientService = {
+
+                    spec = {
+
+                        ports = [
+
+                            {
+
+                                name     = "prometheus"
+                                protocol = "TCP"
+                                port     = 15692
+
+                            }
+
+                        ]
+
+                    }
+
+                }
+
+                statefulSet = {
+
+                    spec = {
+
+                        template = {
+
+                            spect = {
+
+                                containers = [
+
+                                    {
+
+                                        name = "rabbitmq"
+
+                                        ports = [
+
+                                            {
+
+                                                name          = "prometheus"
+                                                protocol      = "TCP"
+                                                containerPort = 15692
+
+                                            }
+
+                                        ]
+
+                                    }
+
+                                ]
+
+                            }
+
+                        }
+
+                    }
+
+                }
+
+            }
+
+            rabbitmq = {
+
+                additionalPlugins = [ "rabbitmq_management", "rabbitmq_top", "rabbitmq_shovel", "rabbitmq_prometheus" ]
+
+            }
+
+            service = var.service
+
+            persistence = {
+
+                storageClassName = "gp2"
+                storage          = "${ var.storage_gb }Gi"
+
+            }
+
+            resources = {
+
+                requests = {
+
+                    cpu    = var.limit_cpu
+                    memory = var.limit_memory
+
+                }
+
+                limits = {
+
+                    cpu    = var.limit_cpu
+                    memory = var.limit_memory
+
+                }
+
+            }
+
+        }
+
+    }
+
+}
+
+resource "null_resource" "readywait" {
+
+    provisioner "local-exec" {
+
+        command = "sleep 10 && kubectl --server=\"${ var.server }\" --token=\"${ var.token }\" rollout status statefulset/${ var.name }-rabbitmq-server"
+
+    }
+
+}
+
+resource "null_resource" "adduser" {
+
+    depends_on = [ null_resource.readywait ]
+
+    count = length(var.users)
+
+    provisioner "local-exec" {
+
+        command = "kubectl --server=\"${ var.server }\" --token=\"${ var.token }\" exec -n ${ var.namespace } ${ var.name }-rabbitmq-server-0 -it -- rabbitmqctl add_user '${ var.users[ count.index ].username }' '${ var.users[ count.index ].password }' && kubectl exec -n ${ var.namespace } ${ var.name }-rabbitmq-server-0 -it -- rabbitmqctl set_user_tags '${ var.users[ count.index ].username }' '${ var.users[ count.index ].tags }' && kubectl exec -n ${ var.namespace } ${ var.name }-rabbitmq-server-0 -it -- rabbitmqctl set_permissions -p / '${ var.users[ count.index ].username }' ${ var.users[ count.index ].permissions }"
+
+    }
+
+}
---
+++ variables.tf
@@ -0,0 +1,134 @@
+variable "server" {
+
+    type        = string
+    description = "url to cluster api"
+
+}
+
+variable "token" {
+
+    type        = string
+    description = "api token"
+
+}
+
+variable "insecure" {
+
+    type        = bool
+    description = "skip ssl certificate verification"
+    default     = false
+
+}
+
+variable "namespace" {
+
+    type        = string
+    description = "cluster namespace"
+
+}
+
+variable "name" {
+
+    type        = string
+    description = "cluster name"
+
+}
+
+variable "node_selector" {
+
+    type        = map(string)
+    description = "run on on nodes matching selector"
+    default     = null
+
+}
+
+variable "image" {
+
+    type        = string
+    description = "docker image"
+    default     = "rabbitmq:3-management"
+
+}
+
+variable "cookie" {
+
+    type        = string
+    description = "rabbitmq cookie string"
+    default     = "changeme"
+
+}
+
+variable "limit_cpu" {
+
+    type        = string
+    description = "cpu limit"
+    default     = "1000m"
+
+}
+
+variable "limit_memory" {
+
+    type        = string
+    description = "memory limit"
+    default     = "2Gi"
+
+}
+
+variable "users" {
+
+    type = list(object({
+
+        username    = string
+        password    = string
+        vhost       = string
+        tags        = string
+        permissions = string
+
+    }))
+
+    description = "list of users with permissions to create"
+
+}
+
+variable "replicas" {
+
+    type        = number
+    description = "number of replica pods"
+    default     = 1
+
+}
+
+variable "internal_cidr" {
+
+    type        = any
+    description = "restrict loadbalancer to internal cidr"
+    default     = null
+
+}
+
+variable "storage_gb" {
+
+    type        = number
+    description = "storage size in gb"
+    default     = 10
+
+}
+
+variable "service" {
+
+    description = "service annotations"
+
+    default = {
+
+        type = "LoadBalancer"
+
+        annotations = {
+
+            "service.beta.kubernetes.io/aws-load-balancer-type"     = "nlb"
+//            "service.beta.kubernetes.io/aws-load-balancer-internal" = var.internal_cidr
+
+        }
+
+    }
+
+}
