af94407d41b28120150c910e75b0948a0af7edab
Matthew Davis <matthew@matthewdavis.io>
2021-01-02 22:39:25 -0500

    bump

--- main.tf
+++ main.tf
@@ -137,7 +137,9 @@ resource "kubernetes_manifest" "cluster" {
 
                 additionalPlugins = var.additional_plugins
 
-                envConfig = "prometheus.return_per_object_metrics=true"
+                envConfig = <<EOF
+prometheus.return_per_object_metrics = true
+EOF
 
             }
 
