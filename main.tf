5d8bc7205fe8e06541ee815036503e7f167e67ed
Matthew Davis <matthew@matthewdavis.io>
2021-01-02 21:29:49 -0500

    bump

--- main.tf
+++ main.tf
@@ -137,6 +137,11 @@ resource "kubernetes_manifest" "cluster" {
 
                 additionalPlugins = var.additional_plugins
 
+                envConfig = {
+
+                    return_per_object_metrics = true
+
+                }
 
             }
 
